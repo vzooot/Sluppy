f2e347e7c49bb736c7fb6e716afec1d1ef22eabe
vzotikas <vzotikas@vzotikas.com>
2022-12-11 20:46:28 +0100

    Initial commit

---
+++ Sluppy/SluppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SluppyApp.swift
+//  Sluppy
+//
+//  Created by Administrator on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SluppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SluppyTests/SluppyTests.swift
@@ -0,0 +1,15 @@
+//
+//  SluppyTests.swift
+//  SluppyTests
+//
+//  Created by Administrator on 12/11/22.
+//
+
+@testable import Sluppy
+import XCTest
+
+final class SluppyTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(2 * 2, 4)
+    }
+}
